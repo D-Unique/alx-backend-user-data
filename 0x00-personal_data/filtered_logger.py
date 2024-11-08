0c4358b7c3f5982d8e6b256afe9b544bff85728b
Unigwe <emmunigwe@gmail.com>
2024-11-08 02:41:29 +0100

    feat  filtered_logger.py*

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator):
+    """ this function that returns the log message obfuscated:"""
+    for field in fields:
+        reg = rf"({field})=[^{separator}]+"
+        message = re.sub(reg,  rf"\1={redaction}", message)
+    return message
