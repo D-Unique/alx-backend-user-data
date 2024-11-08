938a313c32a942ecadc4daacd8a16edea5889ace
Unigwe <emmunigwe@gmail.com>
2024-11-08 02:45:52 +0100

    typed annotate

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ this function that returns the log message obfuscated:"""
     for field in fields:
         reg = rf"({field})=[^{separator}]+"
