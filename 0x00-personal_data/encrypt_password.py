36de1cd7c9a200846ec441d1890da0791fb53b9c
Unigwe <emmunigwe@gmail.com>
2024-11-11 09:18:06 +0100

    added is_valid feat

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,13 @@
 import bcrypt
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> bcrypt.hashpw:
     """this func returns a hashed password"""
     byte = password.encode('utf-8')
     return bcrypt.hashpw(byte, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    passwordbyte = password.encode('utf-8')
+    result = bcrypt.checkpw(passwordbyte, hashed_password)
+    return result
