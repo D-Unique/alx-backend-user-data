3882a88c4580ec370d1f2be1fddc553d4078edb7
Unigwe <emmunigwe@gmail.com>
2024-11-11 09:20:40 +0100

    fix: annotation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,13 +3,14 @@
 import bcrypt
 
 
-def hash_password(password: str) -> bcrypt.hashpw:
+def hash_password(password: str) -> bytes:
     """this func returns a hashed password"""
     byte = password.encode('utf-8')
     return bcrypt.hashpw(byte, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """this function validate the password"""
     passwordbyte = password.encode('utf-8')
     result = bcrypt.checkpw(passwordbyte, hashed_password)
     return result
