2997e4afa8734ebda6afd63026976fcae735352b
Unigwe <emmunigwe@gmail.com>
2024-11-11 08:52:54 +0100

    test main feat

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,27 @@ import mysql.connector as mc
 import os
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format"""
+        message = super().format(record)
+        message = filter_datum(fields=self.fields,
+                               redaction=RedactingFormatter.REDACTION,
+                               message=message,
+                               separator=RedactingFormatter.SEPARATOR)
+        return message
+
+
 def get_db() -> mc.connection.MySQLConnection:
     """this function helps connect to the database"""
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
@@ -47,22 +68,19 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+def main() -> None:
+    logger = get_logger()
+    try:
+        with get_db() as connection:
+            with connection.cursor as cursor:
+                quary = ('SELECT * FROM users')
+                cursor.execute(quary)
+                rows = cursor.fetchall()
+                redacteddata = filter_datum(PII_FIELDS, "***", rows, ';')
+                logger.info(redacteddata)
+    except Exception as e:
+        logger.error(f"an error {e} occured")
 
-    def __init__(self, fields: List[str]):
-        """init"""
-        self.fields = fields
-        super(RedactingFormatter, self).__init__(self.FORMAT)
 
-    def format(self, record: logging.LogRecord) -> str:
-        """format"""
-        message = super().format(record)
-        message = filter_datum(fields=self.fields,
-                               redaction=RedactingFormatter.REDACTION,
-                               message=message,
-                               separator=RedactingFormatter.SEPARATOR)
-        return message
+if __name__ == "__main__":
+    main()
