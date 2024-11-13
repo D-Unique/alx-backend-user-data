1322a102212a33b9996c44a39066a9d070938dc7
Unigwe <emmunigwe@gmail.com>
2024-11-13 07:49:53 +0100

    test

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """This is the authantication module"""
-from flask import  Request
+from flask import Request
 from typing import TypeVar, List
 
 
@@ -12,15 +12,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """this function checks if the path requires auth"""
-        if (path not in excluded_paths or excluded_paths is None or path is None):
-            return True 
+        if (path not in excluded_paths or
+                excluded_paths is None or path is None):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """the function returns the auth header for a request"""
-        req = Request()
-        if req.headers
-        return None
+        authorization_header = request.headers.get('Authorization')
+        if request is None or authorization_header is None:
+            return None
+        return authorization_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """this function returns the current users"""
