3dfcdae33b5a9bf2958def98cad2386f89d54a7b
Unigwe <emmunigwe@gmail.com>
2024-11-14 22:55:21 +0100

    done with task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,15 @@ from models.user import User
 
 class BasicAuth(Auth):
     """Implement basic authentication"""
-    pass
+    
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """This function extract the base64 part of authorization header"""
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        dc = authorization_header.startswith('Basic ')
+        if not dc:
+            return None
+        else:
+            [basic, byt] = authorization_header.split(' ', 1)
+            return byt
