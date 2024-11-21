fb1894a21d12be09b3b4ad9487240c95420cf32c
Unigwe <emmunigwe@gmail.com>
2024-11-21 19:43:21 +0100

    test3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,10 +31,11 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: bytes) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
+        self._session.refresh(new_user)
         return new_user
