e684fde99b6a8b8b92babb8d09607b38b28bfdc6
Unigwe <emmunigwe@gmail.com>
2024-11-22 00:38:06 +0100

    done with 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ This module contains the Auth class"""
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -21,6 +21,17 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the provided email and password are valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            bpassword = password.encode('utf-8')
+            hash_password = user.hashed_password
+            return checkpw(password=bpassword, hashed_password=hash_password)
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
