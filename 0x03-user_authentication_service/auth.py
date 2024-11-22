e5ea73f3266e446d8b709acb2495e5e5e998733e
Unigwe <emmunigwe@gmail.com>
2024-11-22 10:04:57 +0100

    test1 register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,62 +34,6 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> str:
-        """Create a new session for the user
-        """
-        try:
-            user = self._db.find_user_by(email=email)
-            user.session_id = _generate_uuid()
-            self._db._session.commit()
-            return user.session_id
-        except NoResultFound:
-            return None
-
-    def get_user_from_session_id(self, session_id: str) -> User:
-        """
-        get user from session id
-        """
-        if session_id:
-            try:
-                user = self._db.find_user_by(session_id=session_id)
-                return user
-            except NoResultFound:
-                return None
-        else:
-            return None
-
-    def destroy_session(self, user_id: int) -> None:
-        """
-        destroys a session
-        """
-        self._db.update_user(user_id, session_id=None)
-        return None
-
-    def get_reset_password_token(self, email: str) -> str:
-        """
-        generate password reset token
-        """
-        try:
-            user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return user.reset_token
-        except NoResultFound:
-            raise ValueError
-
-    def update_password(self, reset_token: str, password: str) -> None:
-        """
-        update password
-        """
-        try:
-            user = self._db.find_user_by(reset_token=reset_token)
-            hsh_pass = _hash_password(password)
-            self._db.update_user(
-                user.id, hashed_password=hsh_pass, reset_token=None)
-            return None
-        except NoResultFound:
-            raise ValueError
-
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password
