0b5ab72db43327e6013e4097b7e415522d5da0a4
Unigwe <emmunigwe@gmail.com>
2024-11-22 10:49:16 +0100

    test3  get_user_from_session_id,destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,7 +44,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Get a user from a session_id"""
         if session_id is None:
             return None
@@ -56,7 +56,9 @@ class Auth:
                 return None
 
     def destroy_session(self, user_id: int) -> None:
-        """Destroy a session"""
+        """
+        destroys a session
+        """
         self._db.update_user(user_id, session_id=None)
         return None
 
