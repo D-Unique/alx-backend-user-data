4e1e235b84593e9e959b1c28da6e29bd096cc079
Unigwe <emmunigwe@gmail.com>
2024-11-22 11:28:17 +0100

    test3 logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module contain the flask app"""
-from flask import Flask, render_template, jsonify, request, abort, redirect, url_for
+from flask import Flask, render_template, jsonify, request, abort
+from flask import redirect, url_for
 from auth import Auth
 
 
@@ -52,7 +53,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        redirect(url_for('home'), code=302)
+        return redirect(url_for('home'), code=302)
     else:
         abort(403)
 
