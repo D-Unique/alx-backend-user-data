8429e09e4e2fc0b398190cb124115ee4c81ac91f
Unigwe <emmunigwe@gmail.com>
2024-11-22 11:23:50 +0100

    test1 logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        redirect('/', code=302)
+        redirect('home', code=302)
     else:
         abort(403)
 
