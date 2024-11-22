4f4cfb375ba63b03c2f003614d53e024db209f19
Unigwe <emmunigwe@gmail.com>
2024-11-22 12:32:54 +0100

    test get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,6 +64,16 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            user.reset_token = uuid
+            return uuid
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password
